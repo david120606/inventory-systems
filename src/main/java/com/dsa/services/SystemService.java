880f56a0945d77a66cb339bd561a7b4cf5e76059
programador31 <programador31@funcionpublica.gob.mx>
2024-12-22 20:44:32 -0600

    feat: implement pagination for system queries
    - Added next and previous page functionality for queries filtered by status.
    - Included cursors (nextCursor and previousCursor) in the API responses.
    - Calculated total records and total pages for paginated responses.
    - Improved efficiency using Keyset Pagination for large datasets.

--- src/main/java/com/dsa/services/SystemService.java
+++ src/main/java/com/dsa/services/SystemService.java
@@ -4,14 +4,20 @@ import com.dsa.entities.schema.System;
 import com.dsa.entities.enums.SystemStatus;
 import com.dsa.exceptions.BusinessException;
 import com.dsa.repositories.SystemRepository;
+import com.dsa.services.pagination.PaginationService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
-public class SystemService {
+public class SystemService extends PaginationService<System> {
 
     private final SystemRepository systemRepository;
 
@@ -45,8 +51,31 @@ public class SystemService {
     }
 
 
-    public List<System> findByStatus(SystemStatus status) {
-        return systemRepository.findByStatus(status);
+    public Map<String, Object> nextPageByStatus(SystemStatus status, Long lastId, int size) {
+        long totalRecords = systemRepository.countByStatus(status);
+        int totalPages = calculateTotalPages(totalRecords, size);
+
+        lastId = (lastId == null) ? 0L : lastId;
+        Pageable pageable = PageRequest.of(0, size);
+        List<System> systems = systemRepository.findNextPageByStatus(status, lastId, pageable);
+
+        return buildResponse(systems, totalRecords, totalPages, size);
+    }
+
+    public Map<String, Object> previousPageByStatus(SystemStatus status, Long firstId, int size) {
+        if (firstId == null) {
+            throw new IllegalArgumentException("Cursor is required for fetching previous pages.");
+        }
+
+        long totalRecords = systemRepository.countByStatus(status);
+        int totalPages = calculateTotalPages(totalRecords, size);
+
+        Pageable pageable = PageRequest.of(0, size);
+        List<System> systems = systemRepository.findPreviousPageByStatus(status, firstId, pageable);
+
+        Collections.reverse(systems);
+
+        return buildResponse(systems, totalRecords, totalPages, size);
     }
 
 }
